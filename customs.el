85dfce74bff5e21be085a472fbef4bd5a0955a15
Gerard Paapu <gerard.paapu@xero.com>
2013-04-26 12:31:29 +1200

    Move the font settings into platform file

--- customs.el
+++ customs.el
@@ -13,10 +13,3 @@
  '(org-html-head-include-default-style nil)
  '(org-html-head-include-scripts nil)
  '(org-html-postamble nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :height 140 :family "Consolas")))))
--- platform/win-32.el
+++ platform/win-32.el
@@ -1,3 +1,6 @@
 (setenv "PATH" (concat "c:\\cygwin\\bin;" (getenv "PATH")))
 (setq python-command "c:/Python27/python.exe")
 (setq shared-folder "~/Users/Gerard.Paapu/Dropbox/")
+
+(custom-set-faces
+ '(default ((t (:inherit nil :height 140 :family "Consolas")))))
